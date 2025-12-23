c6b81082a2d66830884ab521617882ebe9827745
Arthur Lopes <josearthurlda@gmail.com>
2025-12-23 10:09:19 -0300

    Finished the styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import CurrencyConverter from "./components/CurrencyConverter"
 function App() {
 
   return (
-    <div>
+    <div className="bg-[#0B2545] w-[100vw] h-[100vh] flex flex-col justify-center items-center">
       <CurrencyConverter />
     </div>
   )
--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -41,24 +41,24 @@ function CurrencyConverter(){
     }
 
     return(
-        <form>
-            <h1>Currency Converter</h1>
-            <h3>{base.toUpperCase()} to {target.toUpperCase()} converter</h3>
-            <input onChange={HandleChangeInput} value={input} type="number"></input>
-            <h3>Start Currency</h3>
-            <select value={base} onChange={HandleChangeStart}>
+        <form className="bg-[#13315C] w-[45%] h-[65%] rounded-[64px] border-[#134074] border-2 flex flex-col items-center p-[32px]">
+            <h1 className="text-white text-3xl mb-[32px]">Currency Converter</h1>
+            <h3 className="text-white text-xl">{base.toUpperCase()} to {target.toUpperCase()} converter</h3>
+            <input className="w-[80%] h-[44px] indent-3 rounded-md mb-[16px]" onChange={HandleChangeInput} value={input} type="number"></input>
+            <h3 className="text-white text-xl">Start Currency</h3>
+            <select className="w-[80%] h-[44px] indent-3 rounded-md mb-[16px]" value={base} onChange={HandleChangeStart}>
                 <option value="usd">USD</option>
                 <option value="brl">BRL</option>
                 <option value="eur">EUR</option>
             </select>
-            <h3>Target Currency</h3>
-            <select value={target} onChange={HandleChangeTarget}>
+            <h3 className="text-white text-xl">Target Currency</h3>
+            <select className="w-[80%] h-[44px] indent-3 rounded-md mb-[32px]" value={target} onChange={HandleChangeTarget}>
                 <option value="eur">EUR</option>
                 <option value="usd">USD</option>
                 <option value="brl">BRL</option>
             </select>
-            <button onClick={HandleSubmit} type="submit">Convert</button>
-            <h3>Converted currency: {formatter.format(converted)} {target.toUpperCase()}</h3>
+            <button className="w-[50%] bg-white h-[44px] rounded-md mb-[16px]" onClick={HandleSubmit} type="submit">Convert</button>
+            <h3 className="text-white text-xl">Converted currency: {formatter.format(converted)} {target.toUpperCase()}</h3>
         </form>
     )
 }
